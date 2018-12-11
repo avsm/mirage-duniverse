ca9eed33756877130c833b63970d44c28827c816
Anil Madhavapeddy <anil@recoil.org>
2018-12-11 17:40:04 +0000

    Update vendor for ((dir tcpip) (upstream https://github.com/mirage/mirage-tcpip.git)  (ref v3.5.1))

---
+++ ocaml_modules/tcpip/.travis-ci.sh
@@ -0,0 +1,17 @@
+# default tests
+
+wget https://raw.githubusercontent.com/ocaml/ocaml-travisci-skeleton/master/.travis-opam.sh
+bash -ex .travis-opam.sh
+
+# build the examples
+
+export OPAMYES=1
+eval `opam config env`
+
+cd examples/unikernel
+git log --oneline |head -5
+
+opam install mirage
+mirage configure -t $MIRAGE_MODE $FLAGS
+make depend
+make 
